1414d06f491bf16d8b22cb5470d8404a66f335f5
viktor <baranov.viktor.27@gmail.com>
2017-11-23 22:08:04 +0300

    Refactoring

--- assets/javascripts/application/getValidatorsData.js
+++ assets/javascripts/application/getValidatorsData.js
@@ -1,40 +1,11 @@
-function callContractMethod(web3, addr, i, contractAddr, abi, cb, method) {
-	let oraclesContract = attachToContract(web3, abi, contractAddr)
+function callContractMethod(web3, addr, i, contractAddr, abi, method, cb) {
+	let ValidatorsStorage = attachToContract(web3, abi, contractAddr)
     console.log("attach to oracles contract");
-    if (!oraclesContract) {
+    if (!ValidatorsStorage) {
       return cb();
     }
 
-    oraclesContract.methods[method](addr).call(function(err, res) {
+    ValidatorsStorage.methods[method](addr).call(function(err, res) {
     	cb(i, res);
     })
-}
-
-
-function getValidatorFullName(web3, addr, i, contractAddr, abi, cb) {
-	callContractMethod(web3, addr, i, contractAddr, abi, cb, "getValidatorFullName")
-}
-
-function getValidatorStreetName(web3, addr, i, contractAddr, abi, cb) {
-	callContractMethod(web3, addr, i, contractAddr, abi, cb, "getValidatorStreetName")
-}
-
-function getValidatorState(web3, addr, i, contractAddr, abi, cb) {
-	callContractMethod(web3, addr, i, contractAddr, abi, cb, "getValidatorState")
-}
-
-function getValidatorLicenseExpiredAt(web3, addr, i, contractAddr, abi, cb) {
-	callContractMethod(web3, addr, i, contractAddr, abi, cb, "getValidatorLicenseExpiredAt")
-}
-
-function getValidatorDisablingDate(web3, addr, i, contractAddr, abi, cb) {
-	callContractMethod(web3, addr, i, contractAddr, abi, cb, "getValidatorDisablingDate")
-}
-
-function getValidatorZip(web3, addr, i, contractAddr, abi, cb) {
-	callContractMethod(web3, addr, i, contractAddr, abi, cb, "getValidatorZip")
-}
-
-function getValidatorLicenseID(web3, addr, i, contractAddr, abi, cb) {
-	callContractMethod(web3, addr, i, contractAddr, abi, cb, "getValidatorLicenseID")
 }
